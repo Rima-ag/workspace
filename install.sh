4235056029b50b27b3dfad7e06ed2ec4aef00a42
Rima Al Ghossein <rima.alghossein@datadoghq.com>
2024-05-16 07:45:07 +0200

    fix aliases

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ setup-repo terraform-config
 echo export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock >> ~/.zshrc
 echo export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock >> ~/.bashrc
 
+cd dd/dogweb && rake workspaces:setup_workspace
+echo "source ~/.config/dogweb.shellrc" >> ./.bashrc
+echo "source ~/.config/dogweb.shellrc" >> ./.zshrc
